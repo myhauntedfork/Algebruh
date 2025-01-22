0d84f7ef6c68b779d22decd0ba8d9be42fe74b90
Andrew Hinckley <andrewhinckley@Adams-MBP.lan>
2025-01-22 09:32:18 -0600

    Redesign UI

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,13 @@
 void clearTerminal();
 void backToHome();
 void homeDisplay();
+void mathOptions();
+void chemOptions();
+void physicsOptions();
+void startupDisplay();
 
 int main() {
+    startupDisplay();
     while (1) {
         int choice;
         clearTerminal();
@@ -19,59 +24,57 @@ int main() {
         switch (choice) {            
             case 1:
                 clearTerminal();
-                chooseAreaType();
+                mathOptions();
                 backToHome();
                 break;
             case 2:
                 clearTerminal();
-                solveQuadraticEquation();
+                chemOptions();
                 backToHome();
                 break;
             case 3:
                 clearTerminal();
-                performStoichiometryInteractive();
-                backToHome();
-                break;
-            case 4:
-                clearTerminal();
-                solveKinematicEquasion();
-                backToHome();
-                break;
-            case 5:
-                clearTerminal();
-                calculateGravitationalForce();
+                physicsOptions();
                 backToHome();
                 break;
             case 0:
                 clearTerminal();
                 return 0;
+            default:
+                clearTerminal();
+                std::cout << choice << " is an invalid option.\n";
+                backToHome();
+                break;
         }
     }
     return 1;
 }
 
+void startupDisplay() {
+    int choice;
+    std::cout << "          _            _                _\n";
+    std::cout << "    /\\   | |          | |              | |\n";
+    std::cout << "   /  \\  | | __ _  ___| |__  _ __ _   _| |__\n";
+    std::cout << "  / /\\ \\ | |/ _` |/ _ \\ '_ \\| '__| | | | '_ \\\n";
+    std::cout << " / ____ \\| | (_| |  __/ |_) | |  | |_| | | | |\n";
+    std::cout << "/_/    \\_\\_|\\__, |\\___|_.__/|_|   \\__,_|_| |_|\n";
+    std::cout << "             __/ |\n";
+    std::cout << "            |___/\n";
+    std::cout << "\n[1] Start\n";
+    std::cout << "[0] Quit\n>> ";
+    std::cin >> choice;
+    if (choice == 1) {
+        homeDisplay();
+    } else {
+        exit(0);
+    }                                                          
+}
+
 void homeDisplay() {
-    std::cout << "========================\n";
-    std::cout << "        Algebruh\n";
-    std::cout << "========================\n";
-    std::cout << "\n";
-    std::cout << " MATH\n";
-    std::cout << "------------------------\n";
-    std::cout << " [1] Geometrey\n";
-    std::cout << " [2] Quadratic\n";
-    std::cout << "\n";
-    std::cout << " CHEMISTRY\n";
-    std::cout << "------------------------\n";
-    std::cout << " [3] Stoichiometry\n";
-    std::cout << "\n";
-    std::cout << " PHYSICS\n";
-    std::cout << "------------------------\n";
-    std::cout << " [4] Kinematic Equasion\n";
-    std::cout << " [5] Gravitational Force\n";
-    std::cout << "\n";
-    std::cout << " OTHER\n";
-    std::cout << "------------------------\n";
-    std::cout << " [0] - Quit\n\n";
+    std::cout << "[1] MATH\n";
+    std::cout << "[2] CHEMISTRY\n";
+    std::cout << "[3] PHYSICS\n";
+    std::cout << "[0] Quit\n\n";
 }
 
 void backToHome() {
@@ -88,4 +91,70 @@ void clearTerminal() {
     #else 
         system("clear");
     #endif
+}
+
+void mathOptions() {
+    int choice;
+    std::cout << "[1] Geometry\n";
+    std::cout << "[2] Quardratic Equasion\n>> ";
+    std::cin >> choice;
+    switch (choice) {
+        case 1:
+            clearTerminal();
+            chooseAreaType();
+            backToHome();
+            break;
+        case 2:
+            clearTerminal();
+            solveQuadraticEquation();
+            backToHome();
+            break;
+        default:
+            clearTerminal();
+            std::cout << choice << " is an invalid option.\n";
+            backToHome();
+            break;
+    }
+}
+
+void chemOptions() {
+    int choice;
+    std::cout << "[1] Stoichiometry\n>> ";
+    std::cin >> choice;
+    switch (choice) {
+        case 1:
+            clearTerminal();
+            performStoichiometryInteractive();
+            backToHome();
+            break;
+        default:
+            clearTerminal();
+            std::cout << choice << " is an invalid option.\n";
+            backToHome();
+            break;
+    }
+}
+
+void physicsOptions() {
+    int choice;
+    std::cout << " [1] Kinematic Equasion\n";
+    std::cout << " [2] Gravitational Force\n>> ";
+    std::cin >> choice;
+    switch (choice) {
+        case 1:
+            clearTerminal();
+            solveKinematicEquasion();
+            backToHome();
+            break;
+        case 2:
+            clearTerminal();
+            calculateGravitationalForce();
+            backToHome();
+            break;
+        default:
+            clearTerminal();
+            std::cout << choice << " is an invalid option.\n";
+            backToHome();
+            break;
+    }
 }
